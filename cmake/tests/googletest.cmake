81bda4a41635214569cc110a7dfab90f1e147d47
paradox <109912349+ParadoxGene@users.noreply.github.com>
2024-03-03 22:16:49 -0500

    updated build system

--- cmake/tests/googletest.cmake
+++ cmake/tests/googletest.cmake
@@ -1,9 +1,15 @@
-include(FetchContent)
-
-set(gtest_force_shared_crt on)
-FetchContent_Declare(googletest
-    GIT_REPOSITORY "https://github.com/google/googletest.git"
-    GIT_TAG "v1.14.0"
-    SOURCE_DIR "${PROJECT_DIR}/modules/googletest"
-)
-FetchContent_Populate(googletest)
+function(paradox_add_google_tests_lib version)
+    get_property(GOOGLE_TESTS_LIB_EXISTS GLOBAL PROPERTY GOOGLE_TESTS_LIB_EXISTS_PROPERTY)
+    if(NOT GOOGLE_TESTS_LIB_EXISTS AND PARADOX_LANGUAGE STREQUAL "cpp")
+        set_property(GLOBAL PROPERTY GOOGLE_TESTS_LIB_EXISTS_PROPERTY ON)
+        include(FetchContent)
+        set(gtest_force_shared_crt on)
+        FetchContent_Declare(
+            googletest
+            GIT_REPOSITORY "https://github.com/google/googletest.git"
+            GIT_TAG ${version}
+            SOURCE_DIR "${PROJECT_DIR}/modules/googletest")
+        FetchContent_Populate(googletest)
+        add_subdirectory("${PROJECT_DIR}/modules/googletest" "${CMAKE_BINARY_DIR}/../../unit-tests/googletests/")
+    endif()
+endfunction()
--- cmake/tests/unity.cmake
+++ cmake/tests/unity.cmake
@@ -1,8 +1,15 @@
-include(FetchContent)
+function(paradox_add_unity_testing_lib version)
+    get_property(UNITY_TESTING_LIB_EXISTS GLOBAL PROPERTY UNITY_TESTING_LIB_EXISTS_PROPERTY)
+    if(NOT UNITY_TESTING_LIB_EXISTS AND PARADOX_LANGUAGE STREQUAL "c")
+        set_property(GLOBAL PROPERTY UNITY_TESTING_LIB_EXISTS_PROPERTY ON)
+        include(FetchContent)
+        FetchContent_Declare(
+            Unity
+            GIT_REPOSITORY "https://github.com/ThrowTheSwitch/Unity.git"
+            GIT_TAG "${version}"
+            SOURCE_DIR "${PROJECT_DIR}/modules/Unity")
+        FetchContent_Populate(Unity)
+        add_subdirectory("${PROJECT_DIR}/modules/Unity" "${CMAKE_BINARY_DIR}/../../unit-tests/unity/")
+    endif()
+endfunction()
 
-FetchContent_Declare(Unity
-    GIT_REPOSITORY "https://github.com/ThrowTheSwitch/Unity.git"
-    GIT_TAG "v2.5.2" 
-    SOURCE_DIR "${PROJECT_DIR}/modules/Unity"
-)
-FetchContent_Populate(Unity)
