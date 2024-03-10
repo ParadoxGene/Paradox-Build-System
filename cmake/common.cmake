0568888e5293302c1b24eb1f4260b2e326fdc7e6
Paradox Gene <109912349+ParadoxGene@users.noreply.github.com>
2024-03-10 13:21:21 -0400

    updated the build system

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,7 +1,7 @@
-function(paradox_set_global varname value)
+macro(paradox_set_global varname value)
     set_property(GLOBAL PROPERTY PARADOX_GLOBAL_${varname}_PROPERTY ${value})
-endfunction()
+endmacro()
 
-function(paradox_get_global varname)
+macro(paradox_get_global varname)
     get_property(PARADOX_GLOBAL_${varname} GLOBAL PROPERTY PARADOX_GLOBAL_${varname}_PROPERTY)
-endfunction()
+endmacro()
